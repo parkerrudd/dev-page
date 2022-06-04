f505f7a21aee5884bf06d3d03791fa6e5c262066
Parker Rudd <parkerbrudd@gmail.com>
2022-06-04 14:58:23 -0600

    added to new proj

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import styles from './styles/style.css'; 
+import logo from './images/dev-logo-lighter-bg.png'; 
+import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa"; 
+import WorkPage from './workpage';
+import AboutPage from './aboutpage';
+import Contact from './contact';
+
+function App() {
+
+  return (
+    <div className="App">
+       <section className="home" id='home'>
+         <div className="nav">
+            <a className="logo" href="#home">
+              <img id="logo-image" src={logo} alt="" />
+            </a>
+            <div className="links">
+              <a id="about" href="#aboutpage">About</a>
+              <a id="work" href="#workpage">Work</a>
+              <a id="contact" href="#contactpage">Contact</a>
+            </div>
+         </div>
+
+         <div className="info-section">
+            <h1 className="blue-font">Hey, I'm Parker.</h1>
+            <h1 className="darker-font">I'm a web developer.</h1>
+            <p id="info-paragraph">I'm a software engineer passionate about combining the art of design and the skill of programming to make creative user experiences that are enjoyable and easy to use.</p>
+
+            <div className="icons">
+              <a id='github-logo' href="https://github.com/parkerrudd" target="_blank"> <FaGithub /> </a>
+              <a id='linkedin-logo' href="https://www.linkedin.com/in/parkerrudd" target="_blank"> <FaLinkedin /> </a>
+              <a id='twitter-logo' href="https://twitter.com/parkerrudd" target="_blank"> <FaTwitter /> </a>
+            </div>
+
+          </div>
+
+       </section>
+
+      <AboutPage /> 
+      
+      <WorkPage />
+
+      <Contact />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/aboutpage.js
@@ -0,0 +1,40 @@
+import styles from './styles/style.css'; 
+import headshot from './images/headshot2.png'; 
+
+function AboutPage() {
+
+    return (
+        <section id='aboutpage'>
+            <div className="about-container">
+                <div className="about-words">
+                    <h3 className='m-plus'>About Me</h3>
+                    <hr/>
+                    <p className='m-plus'>Hello! My name is Parker and I love creating enjoyable and interesting user experiences. My developer journey began in late 2021, when I wanted to make a website for my music. I started learning  web development, and in doing so, realized how much I enjoyed coding. Before long I decided to work towards becoming a software engineer. 
+                    </p>
+                    <p className='m-plus'>Since then I've built multiple applications and websites, including a third party Spotify app that randomly generates a song for you based on your current mood. Being a self taught developer, I am highly confident in my problem solving and troubleshooting abilities, and I'm always looking for more technologies to learn in order to create the best user experience possible. </p>
+                    <h4 className='m-plus'>Here are some languages I speak:</h4>
+                    <div className="languages">
+                        <ul>
+                            <div className="top-ul">
+                                <li>HTML</li>
+                                <li>CSS/SASS</li>
+                            </div>
+                            <div className="bottom-ul">
+                                <li>JavaScript</li>
+                                <li>React</li>
+                            </div>
+                        </ul>
+                    </div>
+                    <a href="https://docs.google.com/document/d/1bYgoX9_E0shY8UNCMidPCNN24379dtFYJ6oHC696_6Y/edit?usp=sharing" target="_blank"><button className='m-plus' id='resume-btn'>Resume</button></a>
+                </div>
+
+                <div className="headshot-container">
+                    <img id='headshot' src={headshot} alt="" />
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+export default AboutPage; 
